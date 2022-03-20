b9248c3154598862a7c7d70d00425c991062327f
TheRealRondon <amirhossein2324236@gmail.com>
2022-03-20 18:12:01 +0330

    Block creation added.

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
-	"fmt"
-
-	"rsc.io/quote"
+	"bytes"
+	"crypto/sha256"
 )
 
+type BlockChain struct {
+	block []*Block
+}
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
 func main() {
-	fmt.Println(quote.Hello())
 }
